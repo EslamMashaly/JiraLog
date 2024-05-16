c9a7b3fcc94710c3124d6abb2b6bb93a34e4d56d
EslamMashaly <eslam.mashaly217@gmail.com>
2024-05-16 15:33:37 +0300

    final

--- src/test/java/Log.java
+++ src/test/java/Log.java
@@ -2,7 +2,6 @@ import dataReaders.ExcelReader;
 import dataReaders.PropertiesReader;
 import io.restassured.parsing.Parser;
 import org.testng.annotations.Test;
-import pojo.LogInfo;
 import pojo.LoginResponse;
 import utils.DateFormat;
 
@@ -12,7 +11,6 @@ import static io.restassured.RestAssured.*;
 
 public class Log extends ExcelReader {
     String token;
-    String workLogID;
 
     @Test(dataProvider = "Credentials")
     public void authenticate(String ntAccount, String password) {
@@ -35,6 +33,7 @@ public class Log extends ExcelReader {
 
     @Test(dataProvider = "Logging")
     public void logHours(String date, String time, String storyID, String description) throws ParseException {
+
         String loggedDate = DateFormat.DateFormatter(date);
 
         Double timeInDouble = Double.parseDouble(time) * 3600;
@@ -42,7 +41,7 @@ public class Log extends ExcelReader {
         System.out.println(timeInDouble);
 
         baseURI = PropertiesReader.Data.getProperty("URL");
-        LogInfo li = given().relaxedHTTPSValidation().body("{\n" +
+         given().relaxedHTTPSValidation().body("{\n" +
                         "    \"comment\": \"" + description + "\",\n" +
                         "    \"started\": \"" + loggedDate + "T12:15:16.746+0000\",\n" +
                         "    \"timeSpentSeconds\": \"" + timeInSeconds + "\"\n" +
@@ -51,33 +50,9 @@ public class Log extends ExcelReader {
                 .header("Cookie", token)
                 .log().all()
                 .pathParam("storyID", storyID)
-                .expect().defaultParser(Parser.JSON)
                 .when().post("/rest/api/2/issue/{storyID}/worklog")
-                //        workLogID =JsonParser.parse(res,"id");
                 .then().log().all()
-                .assertThat().statusCode(201)
-                .extract().response().as(LogInfo.class);
-        workLogID = li.getId();
-//        Log.workLogID2.set(workLogID);
-//        JsonPath jsonPath = new JsonPath(res);
-//        workLogID = jsonPath.getString("id");
-//        System.out.println("id: " + workLogID);
-        //        issueId = jsonPath.getString("issueId");
-//        System.out.println(workLogID);
+                .assertThat().statusCode(201);
+
     }
-
-
-//    @Test(dataProvider = "Logging")
-//    public void deleteWorkLog(String date, String time, String storyID, String description) {
-//        String extractedValueFromFirstTest = Log.workLogID2.get();
-//        baseURI = PropertiesReader.Data.getProperty("URL");
-//        given().relaxedHTTPSValidation()
-//                .header("Content-Type", "application/json")
-//                .header("Cookie", token)
-//                .pathParam("storyID", storyID)
-//                .pathParam("id", extractedValueFromFirstTest)
-//                .when().delete("/rest/api/2/issue/{storyID}/worklog/{id}")
-//                .then().log().body()
-//                .assertThat().statusCode(204);
-//    }
 }
